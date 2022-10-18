d4367b85f6c0559a34e310acd09f464b7c10b7e5
Evan Cazares <schmoopy@Evans-Air-2.attlocal.net>
2022-10-18 12:56:57 -0500

    get count i think done, retunrs in correct number since add node is wrong

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,11 +14,11 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
     // if adding at end, next needs to be null, if at the start prev needs to be null
     // find duplicates
     bool duplicate = false, sucess;
-    if( id > 0 && !ptr->empty()){ // if id is 0 or greater and string is not empty...
-
+    if( id > -1 && !ptr->empty()){ // if id is 0 or greater and string is not empty...
+        
         Node* newNode = new Node; // allocated memory for newnode struct
-        newNode->data.id = id; // set data obj id in the new node to id
-        newNode->data.data = *ptr; // set data obj data in the new node to dereference of ptr as it needs to own it
+        //newNode->data.id = id; // set data obj id in the new node to id
+        //newNode->data.data = *ptr; // set data obj data in the new node to dereference of ptr as it needs to own it
 
         if (head == NULL){ // determine if we are adding at the beggining
             head = newNode;
@@ -28,15 +28,7 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
         } else if(head != NULL){ // if not the beggining...
             Node* current = head; // create postion marker and set postion as first node
             while(current->next != NULL){ // loop through list, comparing id's until thingy is found, if not found and no duplicate , add at end
-                if(current->data.id > id && current->data.id != id) { // if the current node's id is greater than ours and no duplicates have been found...
-                    // flag boolean value checked to prevent excess running
-                    newNode->next = current; // since current is after we want the next pointer in our new node to point to it
-                    newNode->prev = current->prev; // simiar, set pointer to previous node to the pointer of the previous node stored in the current node
-                    current->prev->next = newNode; // set the node pointer next in the node before current to our new node
-                    current->prev = newNode; // set prev point in the current node to our inserted node
-                    sucess = true;
-                    break; // break loop since we found it and dont want to run duplicate if statment
-                } else if (current->data.id == id){ // if duplicate...
+                if (current->data.id == id){ // if duplicate...
                     // retunr false, set duplicate to true
                     duplicate = true;
                     sucess = false;
@@ -45,6 +37,16 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
                     // but if statment not used cuz dplicate = true so we can break;
                     // logic error maybe
                 }
+                if(current->data.id > id) { // if the current node's id is greater than ours and no duplicates have been found...
+                    // flag boolean value checked to prevent excess running
+                    newNode->next = current; // since current is after we want the next pointer in our new node to point to it
+                    newNode->prev = current->prev; // simiar, set pointer to previous node to the pointer of the previous node stored in the current node
+                    current->prev->next = newNode; // set the node pointer next in the node before current to our new node
+                    current->prev = newNode; // set prev point in the current node to our inserted node
+                    sucess = true;
+                    duplicate = true; // since we did find and dont want if statment to run
+                    break; // break loop since we found it and dont want to run duplicate if statment
+                } 
                 current = current->next; // go to next node
             }
             // if it was never found and no duplicates, add at end
@@ -59,7 +61,7 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
     return sucess;
 }
 
-bool LinkedList::deleteNode(int id){
+bool LinkedList::deleteNode(int id){ // wrong
     Node* current = head;
     bool success = false;
     while(current->next != NULL){
@@ -85,6 +87,7 @@ bool LinkedList::getNode(int id, Data* data){
             data->id = current->data.id;
             success = true;
         }
+        current = current->next;
     }
     if (success == false){
         data->data = "";
@@ -94,22 +97,40 @@ bool LinkedList::getNode(int id, Data* data){
 }
 
 void LinkedList::printList(bool backward){
-    std::cout << "Test&" << std::endl;
-    return;
+    Node* current = head;
+    if(current == NULL){
+        return; // only one return statment, stops cuz nothing to print
+        
+    } else if(backward == true) { // set current to the end and go backwards
+        while(current->next != NULL){
+            current = current->next;
+        }
+        while(current->prev != NULL){
+            std::cout << current->data.id << " : " << current->data.data << std::endl;
+            current = current->prev;
+        }
+    } else if (backward == false){ // just start at begginnig
+        while(current->next != NULL){
+            std::cout << current->data.id << " : " << current->data.data << std::endl;
+            current = current->next;
+        }
+    }
 }
 
 int LinkedList::getCount(){
-    int counter = 0;
-    // Node* current;
-    // if (head != NULL){
-    //     current = head;
-    //     while(current->next != NULL){
-    //     counter++;
-    //     }
-    // }
+    Node* current = head;
+    int counter;
+    if(current == NULL){
+        counter = 0;
+    } else {
+        while(current->next != NULL){
+            counter++;
+            current = current->next;
+        }
+    }
     return counter;
-
 }
+
 bool LinkedList::clearList(){
     return false;
 }
