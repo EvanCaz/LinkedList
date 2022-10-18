7c4465c2afb8d83b775aabaf0242688c87e596f1
Evan Cazares <schmoopy@Evans-Air-2.attlocal.net>
2022-10-17 22:53:46 -0500

    Stubbed out methods and it compiles

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,32 @@
-/*
- * your comment header here
- */
+/*********************
+ * Evan Cazares
+ * Assignment: coding06
+* doubly linked list with a head, no tail, and proper methods
+**********************/
+
+#include "linkedlist.h"
+
+bool LinkedList::addNode(int id, string* ptr){
+    return false;
+}
+bool LinkedList::deleteNode(int id){
+    return false;
+}
+
+bool LinkedList::getNode(int id, Data* data){
+    return false;
+}
+
+void LinkedList::printList(bool backward){
+    std::cout << "Test" << std::endl;
+}
+
+int LinkedList::getCount(){
+    return 0;
+}
+bool LinkedList::clearList(){
+    return false;
+}
+bool LinkedList::exists(int id){
+    return false;
+}
--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,42 @@
-/*
- * your comment header here
- */
+/*********************
+ * Evan Cazares
+ * Assignment: coding06
+* doubly linked list with a head, no tail, and proper methods
+**********************/
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "data.h"
+#include <iostream>
+
+
+class LinkedList {
+    private:
+    Node *head; // pointer for first node, null when empty list/no list
+    public:
+    // addNode, deleteNode, getNode, printList, getCount, clearList, exists
+    // construcot and destrucot to allocate and deallocate memory
+    bool addNode(int, string*);
+    // hard, have to leave in sorted order, reattach, input validation, check to make sure there are no dupiclates and delete if so,
+    // should be called insertNode
+
+    bool deleteNode(int); // delete the node with specifed int, reattaching the prev and next nodes togethor
+    bool getNode(int, Data*); 
+    // just like pop or peek, pass method the node you want and an empty struct to hold the data in that node, get data, return that struct
+    // and retunr true, if not found fill empty struct and return false
+    void printList(bool = false);
+    //print the list either forward or backward dependent on flag, defualt is forward
+    int getCount();
+    // get count each time called
+    bool clearList();
+    // deallocated and set head to null instead of the first node
+    bool exists(int);
+    // checks to see if given id corresponds with a node in the list
+
+};
+
+
+
+
+#endif
