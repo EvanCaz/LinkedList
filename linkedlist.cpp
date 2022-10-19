c19a907e4539eaa1c971f8762d8608b0f912b277
Evan Cazares <schmoopy@Evans-Air-2.attlocal.net>
2022-10-19 14:33:24 -0500

    kinda working addNode method

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -13,50 +13,47 @@ LinkedList::LinkedList(){
 bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are creating first node in list or just creating next one
     // if adding at end, next needs to be null, if at the start prev needs to be null
     // find duplicates
-    bool duplicate = false, sucess = false;
+    bool duplicate = false, sucess = false, found = false, beggining = false;
     if( id > -1 && !ptr->empty()){ // if id is 0 or greater and string is not empty...
-        
-        Node* newNode = new Node; // allocated memory for newnode struct
-        //newNode->data.id = id; // set data obj id in the new node to id
-        //newNode->data.data = *ptr; // set data obj data in the new node to dereference of ptr as it needs to own it
-
-        if (head == NULL){ // determine if we are adding at the beggining
-            head = newNode;
-            newNode->prev = NULL; // nothing before it
-            newNode->next = NULL; // since its the first and only one
-            sucess = true; 
-        } else { // if not the beggining...
-            Node* current = head; // create postion marker and set postion as first node
-            while(current->next != NULL){ // loop through list, comparing id's until thingy is found, if not found and no duplicate , add at end
-                if (current->data.id == id){ // if duplicate...
-                    // retunr false, set duplicate to true
+        Node* current = head; // set a marker
+        if (current == NULL){ // if we are at the begininng set a flag to true
+            beggining = true;
+        } else { // if not beginning
+            while(current->next != NULL){  // repeat until marker is last node if not found things
+                if (current->data.id == id){
                     duplicate = true;
                     sucess = false;
                     break;
-                    // we dont want to break because we want current to be set to the end node to be used in next if statment
-                    // but if statment not used cuz dplicate = true so we can break;
-                    // logic error maybe
                 }
-                if(current->data.id > id) { // if the current node's id is greater than ours and no duplicates have been found...
-                    // flag boolean value checked to prevent excess running
-                    newNode->next = current; // since current is after we want the next pointer in our new node to point to it
-                    newNode->prev = current->prev; // simiar, set pointer to previous node to the pointer of the previous node stored in the current node
-                    current->prev->next = newNode; // set the node pointer next in the node before current to our new node
-                    current->prev = newNode; // set prev point in the current node to our inserted node
-                    sucess = true;
-                    duplicate = true; // since we did find and dont want if statment to run
-                    break; // break loop since we found it and dont want to run duplicate if statment
-                } 
-                current = current->next; // go to next node
-            }
-            // if it was never found and no duplicates, add at end
-            if(duplicate == false){
-                newNode->next = NULL;
-                newNode->prev = current; // set to last node
-                current->next = newNode; // set last node next pointer to our end
-                sucess = true;
+                if(current->data.id > id){
+                    found = true;
+                    break;
+                }
+                current = current->next;
             }
         }
+            if(duplicate == false){
+                Node* newNode = new Node;
+                newNode->data.id = id;
+                newNode->data.data = *ptr;
+                if (beggining == true){ // adding new head
+                    head = newNode;
+                    newNode->prev = NULL;
+                    newNode->next = NULL;
+                    sucess = true;
+                } else if(beggining == false && found == true){ // found inside the list
+                    newNode->next = current;
+                    newNode->prev = current->prev;
+                    current->prev->next = newNode;
+                    current->prev = newNode;
+                    sucess = true;
+                } else if (beggining == false && found == false){// adding tail
+                    newNode->prev = current;
+                    newNode->next = NULL;
+                    current->next = newNode;
+                    sucess = true;
+                }
+            }
     }
     return sucess;
 }
@@ -104,7 +101,7 @@ void LinkedList::printList(bool backward){
     } else if(backward == true) { // set current to the end and go backwards
         while(current->next != NULL){
             current = current->next;
-        }
+        }    
         while(current->prev != NULL){
             std::cout << current->data.id << " : " << current->data.data << std::endl;
             current = current->prev;
