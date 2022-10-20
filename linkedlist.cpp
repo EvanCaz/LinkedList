154ecbcf2438091df76dcbddc09763007fe7c8a5
Evan Cazares <schmoopy@Evans-Air-2.attlocal.net>
2022-10-19 21:06:52 -0500

    unsure deleteNode

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -13,13 +13,13 @@ LinkedList::LinkedList(){
 bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are creating first node in list or just creating next one
     // if adding at end, next needs to be null, if at the start prev needs to be null
     // find duplicates
-    bool duplicate = false, sucess = false, found = false, beggining = false;
-    if( id > -1 && !ptr->empty()){ // if id is 0 or greater and string is not empty...
+    bool duplicate = false, sucess = false, found = false, beggining = false; // use flags to allocate memory after determining no duplicate
+    if( id > 0 && !ptr->empty()){ // if id is 0 or greater and string is not empty...
         Node* current = head; // set a marker
         if (current == NULL){ // if we are at the begininng set a flag to true
             beggining = true;
         } else { // if not beginning
-            while(current->next != NULL){  // repeat until marker is last node if not found things
+            while(current->next != NULL){  // repeat until marker is last node if not found id
                 if (current->data.id == id){
                     duplicate = true;
                     sucess = false;
@@ -48,10 +48,15 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
                     current->prev = newNode;
                     sucess = true;
                 } else if (beggining == false && found == false){// adding tail
-                    newNode->prev = current;
+                    if (current->data.id == id){ // need this incase list has only one node and next id is a duplicate, logic flaw not sure how to solve
+                    duplicate = true;
+                    sucess = false;
+                    } else {
+                        newNode->prev = current;
                     newNode->next = NULL;
                     current->next = newNode;
                     sucess = true;
+                    }
                 }
             }
     }
@@ -61,16 +66,18 @@ bool LinkedList::addNode(int id, string* ptr){ // needs to deterine if we are cr
 bool LinkedList::deleteNode(int id){ // wrong
     Node* current = head;
     bool success = false;
-    while(current->next != NULL){
-        if (current->data.id == id){
-            current->prev->next = current->next; // set the node before currents next pointer to the node after current
-            current->next->prev = current->prev; // set the node after currents prev pointer to the node before current
-            delete current->next;
-            delete current->prev; // dont think i need this as i did not allocate memory for pointers in the struct, maybe just delete current
-            delete current;
-            success = true;
-        }
-        current = current->next;
+    if(id > -1){
+        do
+        {
+            if(current->data.id == id){
+                delete current->prev;
+                delete current->next;
+                delete current;
+                success = true;
+                break;
+            }
+            current = current->next;
+        } while (current->next != NULL);
     }
     return success;
 }
@@ -78,54 +85,37 @@ bool LinkedList::deleteNode(int id){ // wrong
 bool LinkedList::getNode(int id, Data* data){
     Node* current = head;
     bool success = false;
-    while(current->next != NULL){
-        if(current->data.id == id){
-            data->data = current->data.data;
-            data->id = current->data.id;
-            success = true;
-        }
-        current = current->next;
-    }
-    if (success == false){
-        data->data = "";
-        data->id = -1;
-    }
+    // while(current->next != NULL){
+    //     if(current->data.id == id){
+    //         data->data = current->data.data;
+    //         data->id = current->data.id;
+    //         success = true;
+    //     }
+    //     current = current->next;
+    // }
+    // if (success == false){
+    //     data->data = "";
+    //     data->id = -1;
+    // }
     return success;
 }
 
 void LinkedList::printList(bool backward){
-    Node* current = head;
-    if(current == NULL){
-        return; // only one return statment, stops cuz nothing to print
-        
-    } else if(backward == true) { // set current to the end and go backwards
-        while(current->next != NULL){
-            current = current->next;
-        }    
-        while(current->prev != NULL){
-            std::cout << current->data.id << " : " << current->data.data << std::endl;
-            current = current->prev;
-        }
-    } else if (backward == false){ // just start at begginnig
-        while(current->next != NULL){
-            std::cout << current->data.id << " : " << current->data.data << std::endl;
-            current = current->next;
-        }
-    }
+    
 }
 
 int LinkedList::getCount(){
     Node* current = head;
-    int counter;
+    int nodes = 0;
     if(current == NULL){
-        counter = 0;
+        nodes = 0;
     } else {
-        while(current->next != NULL){
-            counter++;
+        while (current->next != NULL){
+            nodes++;
             current = current->next;
         }
     }
-    return counter;
+    return nodes;
 }
 
 bool LinkedList::clearList(){
